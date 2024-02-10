f1b9b6240b3ac9a22261d78b8e86a140d9877cf9
corentin-desilles <corentindesilles974@gmail.com>
2024-02-10 12:52:19 +0100

    replace pointsMat by shaderMat

--- src/script.js
+++ src/script.js
@@ -97,12 +97,26 @@ const generateGalaxy = () => {
     /**
      * Material
      */
-    material = new THREE.PointsMaterial({
-        size: parameters.size,
-        sizeAttenuation: true,
+    material = new THREE.ShaderMaterial({
         depthWrite: false,
         blending: THREE.AdditiveBlending,
         vertexColors: true,
+        vertexShader: `
+        void main() {
+            vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+            vec4 viewPosition = viewMatrix * modelPosition;
+            vec4 projectedPosition = projectionMatrix * viewPosition;
+            gl_Position = projectedPosition;
+
+            gl_PointSize = 2.0;
+        }
+        `,
+        fragmentShader: `
+        void main() {
+            gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+            #include <colorspace_fragment>
+        }
+        `,
     })
 
     /**
