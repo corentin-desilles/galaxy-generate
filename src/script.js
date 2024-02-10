d6a3634a51bf47a3e38804ec6c9c68400254c26b
corentin-desilles <corentindesilles974@gmail.com>
2024-02-10 13:44:02 +0100

    final color

--- src/script.js
+++ src/script.js
@@ -113,6 +113,7 @@ const generateGalaxy = () => {
         vertexShader: `
         uniform float uSize;
         attribute float aScale;
+        varying vec3 vColor;
         
         void main() {
 
@@ -125,9 +126,14 @@ const generateGalaxy = () => {
             // Size
             gl_PointSize = uSize * aScale;
             gl_PointSize *= (1.0 / - viewPosition.z);
+
+            // Size
+            vColor = color;
         }
         `,
         fragmentShader: `
+        varying vec3 vColor;
+
         void main() {
 
             // // Disc
@@ -145,7 +151,10 @@ const generateGalaxy = () => {
             strength = 1.0 - strength;
             strength = pow(strength, 10.0);
 
-            gl_FragColor = vec4(vec3(strength), 1.0);
+            // Final color
+            vec3 color = mix(vec3(0.0), vColor, strength);
+            
+            gl_FragColor = vec4(color, 1.0);
             #include <colorspace_fragment>
         }
         `,
