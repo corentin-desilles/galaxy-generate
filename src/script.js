d9e8c1c42851e9a2ef11e082149911fbfbc62be8
corentin-desilles <corentindesilles974@gmail.com>
2024-02-10 11:42:22 +0100

    position and shape

--- src/script.js
+++ src/script.js
@@ -20,6 +20,9 @@ const scene = new THREE.Scene()
 const parameters = {}
 parameters.count = 100000
 parameters.size = 0.01
+parameters.radius = 5
+parameters.branches = 3
+
 let geometry = null
 let material = null
 let points = null
@@ -42,9 +45,14 @@ const generateGalaxy = () => {
     for (let i = 0; i < parameters.count; i++) {
         const i3 = i * 3
 
-        positions[i3 + 0] = (Math.random() - 0.5) * 3
-        positions[i3 + 1] = (Math.random() - 0.5) * 3
-        positions[i3 + 2] = (Math.random() - 0.5) * 3
+        //position the particles on the branches
+        const radius = Math.random() * parameters.radius
+        const branchAngle =
+            ((i % parameters.branches) / parameters.branches) * Math.PI * 2
+
+        positions[i3 + 0] = Math.cos(branchAngle) * radius
+        positions[i3 + 1] = 0
+        positions[i3 + 2] = Math.sin(branchAngle) * radius
     }
 
     geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
@@ -77,6 +85,16 @@ gui.add(parameters, 'size')
     .min(0.001)
     .max(0.1)
     .step(0.001)
+    .onFinishChange(generateGalaxy)
+gui.add(parameters, 'radius')
+    .min(0.01)
+    .max(20)
+    .step(0.01)
+    .onFinishChange(generateGalaxy)
+gui.add(parameters, 'branches')
+    .min(2)
+    .max(20)
+    .step(1)
     .onFinishChange(generateGalaxy)
 
 /**
